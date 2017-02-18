c52e07407749da063474211f27d87905edcef52d
Jan Horacek <horacj10@fit.cvut.cz>
2017-02-18 12:21:13 +0100

    Add resources folder
     add resources folder with icons and translate files

---
+++ Program/resources/translate/translate_cs.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+translate = {
+    'Menu_file' : '&Soubor',
+    'Menu_exit' : '&Konec',
+    'Menu_open' : '&Otevřít',
+    'Menu_help' : '&Nápověda',
+    'Menu_about': '&O aplikaci',
+}
