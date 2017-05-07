a674ce16f5f9a8b6ed84113a0f8cb194c9d976c1
Jan Horáček <horacj10@fit.cvut.cz>
2017-05-07 17:17:55 +0200

    translated version of translate

---
+++ Program/resources/translate/translate_en.py
@@ -0,0 +1,277 @@
+# -*- coding: utf-8 -*-
+
+translate = {
+    'Menu_file'                 : '&File',
+    'Menu_exit'                 : '&Exit',
+    'Menu_open'                 : '&Open',
+    'Menu_help'                 : '&Help',
+    'Menu_about'                : '&About',
+    'Menu_save_as'              : 'Save as',
+    'Menu_save'                 : 'Save',
+    'Menu_settings'             : 'Settings',
+
+    'Menu.spell'                : 'Spell',
+    'Menu.ability'              : 'Ability',
+    'Menu.item'                 : 'Item',
+    'Menu.templates'            : 'Templates',
+    'Menu.effect'               : 'Effect',
+    'Menu.modifier'             : 'Modifier',
+    'Menu.character'            : 'Character',
+    'Menu.monster'              : 'Monster',
+    'Menu.scenario'             : 'Scenario',
+    'Menu.location'             : 'Location',
+    'Menu.ability_context'      : 'Ability context',
+    'Menu.map'                  : 'Map editor',
+
+
+    'Name'                      : 'Name',
+    'Description'               : 'Description',
+    'Mana_cost_initial'         : 'Mana cost initial',
+    'Mana_cost_continual'       : 'Mana cost continual',
+    'Range'                     : 'Range',
+    'Scope'                     : 'Scope',
+    'Cast_time'                 : 'Cast time',
+    'Duration'                  : 'Duration',
+    'Class'                     : 'Class',
+    'Race'                      : 'Race',
+    'Chance'                    : 'Chance',
+    'Weight'                    : 'Weight',
+    'Price'                     : 'Price',
+    'Copper'                    : 'Copper',
+    'Silver'                    : 'Silver',
+    'Gold'                      : 'Gold',
+    'Initiative'                : 'Initiative',
+    'Strength'                  : 'Strength',
+    'Rampancy'                  : 'Rampancy',
+    'RangeLow'                  : 'Range low',
+    'RangeMedium'               : 'Range medium',
+    'RangeHigh'                 : 'Range high',
+    'Defence'                   : 'Defence',
+    'WeaponWeight'              : 'Weapon weight',
+    'Handling'                  : 'Handling',
+    'Length'                    : 'Length',
+    'Size'                      : 'Size',
+    'Quality'                   : 'Quality',
+    'Capacity'                  : 'Capacity',
+    'Amount'                    : 'Amount',
+    'Active'                    : 'Active',
+    'Racial'                    : 'Racial weapon',
+    'WeightA'                   : 'Weight A',
+    'WeightB'                   : 'Weight B',
+    'WeightC'                   : 'Weight C',
+
+
+    'Folder'                    : 'Folder',
+    'NodeObject'                : 'Object',
+    'Type'                      : 'Type',
+    'Code'                      : 'Code',
+    'Select_lang'               : 'Select lang',
+    'Create_new_lang'           : 'Create new lang',
+    'Select_value'              : 'Select value',
+
+
+    'ObjectType.SPELL'          : 'Spells',
+    'ObjectType.ABILITY'        : 'Abilities',
+    'ObjectType.ITEM'           : 'Items',
+    'ObjectType.MODIFIER'       : 'Modifiers',
+    'ObjectType.EFFECT'         : 'Effects',
+    'ObjectType.CHARACTER'      : 'Characters',
+    'ObjectType.MONSTER'        : 'Monsters',
+    'ObjectType.SCENARIO'       : 'Scenarios',
+    'ObjectType.LOCATION'       : 'Locations',
+    'ObjectType.ABILITY_CONTEXT': 'Ability contexts',
+    'ObjectType.MAP'            : 'Maps',
+
+    'Items.GENERIC'             : 'Item',
+    'Items.CONTAINER'           : 'Container',
+    'Items.MELEE_WEAPON'        : 'Melee weapon',
+    'Items.RANGED_WEAPON'       : 'Ranged weapon',
+    'Items.THROWABLE_WEAPON'    : 'Throwable weapon',
+    'Items.ARMOR'               : 'Armor',
+    'Items.MONEY'               : 'Money',
+
+
+    'NodeType.FOLDER'           : 'Folder',
+    'NodeType.OBJECT'           : 'Object',
+
+    # ---------- Classes -----------------------------------------------------
+    'ALCHEMIST'                 : 'Alchemist',
+    'MAGICIAN'                  : 'Magician',
+    'RANGER'                    : 'Ranger',
+    'THIEF'                     : 'Thief',
+    'WARRIOR'                   : 'Warrior',
+
+    # --------- Races -------------------------------------------------------
+    'BARBARIAN'                 : 'Barbarian',
+    'DWARF'                     : 'Dwarf',
+    'ELF'                       : 'Elf',
+    'HOBBIT'                    : 'Habbit',
+    'HUMAN'                     : 'Human',
+    'KROLL'                     : 'Kroll',
+    'KUDUK'                     : 'Kuduk',
+
+    # --------- Alignments -------------------------------------------------------
+    'LAWFUL_GOOD'               : 'Lawful good',
+    'CHAOTIC_GOOD'              : 'Chaotic good',
+    'NEUTRAL'                   : 'Neutral',
+    'CHAOTIC_EVIL'              : 'Chaotic evil',
+    'LAWFUL_EVIL'               : 'Lawful evil',
+
+    # --------- Handling -----------------------------------------------------
+    'ONE_HANDED'                : 'One handed',
+    'TWO_HANDED'                : 'Two handed',
+
+    # --------- Weapon weight ------------------------------------------------
+    'LIGHT'                     : 'Light',
+    'MIDDLE'                    : 'Middle',
+    'HEAVY'                     : 'Heavy',
+
+    # --------- Modifier Target Types ------------------------------------------------
+    'AFFECTS_ITEM'              : 'Item',
+    'AFFECTS_CHARACTER'         : 'Character',
+    'AFFECTS_MELEE_WEAPON'      : 'Melee weapon',
+    'AFFECTS_RANGED_WEAPON'     : 'Ranged weapon',
+    'AFFECTS_THROWABLE_WEAPON'  : 'Throwable weapon',
+    'AFFECTS_ARMOR'             : 'Armor',
+    'AFFECTS_CONTAINER'         : 'Container',
+    'AFFECTS_MONEY'             : 'Money',
+    'AFFECTS_ALL_ITEMS'         : 'All items',
+
+    'CONTAINER_CAPACITY'        : 'Container capacity',
+    'CONTAINER_WEIGHT'          : 'Container weight',
+    'ARMOR_QUALITY'             : 'Armor quality',
+    'ARMOR_SIZE'                : 'Armor size',
+    'WEAPON_STRENGTH'           : 'Weapon strength',
+    'WEAPON_RAMPANCY'           : 'Weapon rampancy',
+    'WEAPON_DEFENCE'            : 'Weapon defence',
+    'WEAPON_INITIATIVE'         : 'Weapon racial',
+    'WEAPON_WEIGHT'             : 'Weapon weight',
+    'WEAPON_MELEE_LENGTH'       : 'Weapon length',
+    'WEAPON_MELEE_HANDLING'     : 'Weapon handling',
+    'WEAPON_RANGED_RANGE_ALL'   : 'Ranged weapon strength (all)',
+    'WEAPON_RANGED_RANGE_LOW'   : 'Ranged weapon strength (low)',
+    'WEAPON_RANGED_RANGE_MEDIUM': 'Ranged weapon strength (medium)',
+    'WEAPON_RANGED_RANGE_LONG'  : 'Ranged weapon strength (long)',
+
+    'STRENGTH'                  : 'Strength',
+    'AGILITY'                   : 'Agility',
+    'TOUGHNESS'                 : 'Toughness',
+    'INTELLIGENCE'              : 'Intelligence',
+    'CHARISMA'                  : 'Charisma',
+    'MOBILITY'                  : 'Mobility',
+    'STRENGTH_BONUS'            : 'Strength bonus',
+    'AGILITY_BONUS'             : 'Agility bonus',
+    'TOUGHNESS_BONUS'           : 'Toughness bonus',
+    'INTELLIGENCE_BONUS'        : 'Intelligence bonus',
+    'CHARISMA_BONUS'            : 'Charisma bonus',
+    'MOBILITY_BONUS'            : 'Mobility bonus',
+    'HEALTH'                    : 'Health',
+    'MANA'                      : 'Mana',
+    'WEIGHT'                    : 'Weight',
+    'HEIGHT'                    : 'Height',
+    'OBSERVATION_RANDOM'        : 'Observation random',
+    'OBSERVATION_SEARCHING'     : 'Observation searching',
+    'OBSERVATION_MECHANISM'     : 'Observation mechanism',
+
+    'TO_TOTAL'                  : '+ to total',
+    'FROM_BASE'                 : '% from base',
+    'FROM_TOTAL'                : '% from total',
+
+    'Effect_target'             : 'Effect target',
+
+    'Delete'                    : 'Delete',
+    'New_item'                  : 'New item',
+    'Import'                    : 'Import',
+    'Rename'                    : 'Rename',
+    'Add_object'                : 'Add another object',
+
+    'New_name'                  : 'New name',
+
+    'tip.New_root_node'         : 'Add root object',
+    'tip.Import_templates'      : 'Import templates',
+    'tip.Export_templates'      : 'Export templates',
+
+    'Target_type'               : 'Modifier target',
+    'Target_attribute_type'     : 'Target attribute',
+    'Value_type'                : 'Value type',
+    'Value'                     : 'Value',
+
+    'File_select_target'        : 'Select target file',
+    'File_select_open'          : 'Select target file',
+
+    'Viability'                 : 'Viability',
+    'Offense'                   : 'Offense',
+    'Defense'                   : 'Defense',
+    'Endurance'                 : 'Endurance',
+    'Mobility'                  : 'Mobility',
+    'Perseverance'              : 'Perseverance',
+    'Intelligence'              : 'Intelligence',
+    'Charisma'                  : 'Charisma',
+    'Alignment'                 : 'Alignment',
+    'Experience'                : 'Experience',
+    'Hp'                        : 'Hp',
+    'Monster_race'              : 'Race of monster',
+    'Monster_size'              : 'Size of monster',
+    'Agility'                   : 'Agility',
+    'Toughness'                 : 'Toughness',
+    'Age'                       : 'Age',
+    'Height'                    : 'Height',
+    'Level'                     : 'Level',
+    'Xp'                        : 'Xp',
+    'MaxHealth'                 : 'Max health',
+    'MaxMana'                   : 'Max mana',
+    'CurrentHealth'             : 'Current health',
+    'CurrentMana'               : 'Current mana',
+    'Date'                      : 'Date',
+    ''                          : '',
+
+    'DRAGON'                    : 'Dragon',
+    'ANIMAL'                    : 'Animal',
+    'INSECT'                    : 'Hmyz',
+    'UNDEAD'                    : 'Nemrtvý',
+    'HUMANOID'                  : 'Humanoidní',
+    'MYTHIC'                    : 'Mýtický',
+    'OTHERS'                    : 'Ostatní',
+
+    'A0'                        : 'Velikost A0',
+    'A'                         : 'Velikost A',
+    'B'                         : 'Velikost B',
+    'C'                         : 'Velikost C',
+    'D'                         : 'Velikost D',
+    'E'                         : 'Velikost E',
+
+    'name'                      : 'Jméno',
+    'description'               : 'Popis',
+
+    'Add_monster'               : 'Přidat příšeru na mapu',
+    'Add_room'                  : 'Přidat místnost na mapu',
+    'Add_object_map'            : 'Přidat objekt na mapu',
+    'Add_item'                  : 'Přidat předmět na mapu',
+
+
+    'Open_map'                  : 'Přidat podkladouvou mapu',
+    'Zoom_in'                   : 'Přiblížit',
+    'Zoom_out'                  : 'Oddálit',
+    'Edit_information'          : 'Upravit informace',
+    'Edit_info'                 : 'Upravit popis mapy',
+
+    'About'                     : 'O aplikaci',
+
+    'Inventory'                 : 'Inventář',
+    'Ground'                    : 'Zem',
+
+
+    'About_text'                : '''
+<p>Aplikace <b>Deskchar</b> slouží jako podpůrná aplikace pro balíček <b>Mobchar</b> pro hru Dračí doupě. 
+Je možné zde vytvářet šablony pro základní balíček pro hráče a také vytvářet rozlehlé dobrodružství pro pány jeskyně.
+Nově je možné vytvářet i jednoduché mapy s popisky.</p>
+<br>        
+<b>Vývojáři</b>:
+<li>
+    <ul>Jan Horáček</ul>
+</li>
+    ''',
+
+    'Quit_text'                 : 'Opravdu chcete ukončit program? <br> Některé vaše práce může být <b>ztracena</b>!',
+    'Sure_quit'                 : 'Opravdu ukončit?'
+}
